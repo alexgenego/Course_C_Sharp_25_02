20d0cbbc8b6c1f16f3015e7f5d662d32d7b2f1fb
Alex <amkulikov@gmail.com>
2023-03-05 00:09:47 +0300

    3_4_hw1

---
+++ Lesson_3/3_4_HW1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1. Напишите программу, которая на вход принимает пятизначное
+// число, а на выход показывает, является-ли оно палиндромом.
+
+void Pal(int num)
+{
+    if (((num / 10000 == num % 10)) && (((num / 1000) % 10 == (num / 10) % 10)))
+    {
+        Console.WriteLine("Palindrome");
+    }
+    else
+    {
+        Console.WriteLine("Not Palindrome");
+    }
+}
+Console.WriteLine("Enter a five-digit number");
+int x = int.Parse(Console.ReadLine()!);
+Pal(x);
