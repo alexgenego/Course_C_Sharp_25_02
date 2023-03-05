38305664d1ff82e1c353c621e116c1797b2b1b74
Alex <amkulikov@gmail.com>
2023-03-05 11:17:24 +0300

    Lesson 4 All

---
+++ Lesson_4/4_0/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, принимающую число N и выдающую сумму от 1 - N
+
+Int SumNum(int num);
+{
+    int all_sum = 0
+    for (i=1, i <= num, i++);
+        all_sum += i;
+    
+    return all_sum
+}
+int A = int.Parse(Console.ReadLine()!);
+int result = SumNum(A);
+Console.WriteLine(result);
