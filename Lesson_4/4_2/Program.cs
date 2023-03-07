a97a86d6d3db370406f6b393dc75941ccfb32a4b
Alex <amkulikov@gmail.com>
2023-03-07 19:37:12 +0300

    4_4_HW1

--- Lesson_4/4_2/Program.cs
+++ Lesson_4/4_2/Program.cs
@@ -8,10 +8,8 @@ int protectnomber(int num)
     int all_pr = 1;
     for (int i = 1; i <= num; i++)
         all_pr *= i;
-
     return all_pr;
 }
-
 int A = int.Parse(Console.ReadLine()!);
 int result = protectnomber(A);
 Console.WriteLine(result);
---
+++ Lesson_4/4_4_HW/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+
+//int Exp(int pr)
+  //      {
+    //        int Exp1 = pr;
+        
+//            for (int A, B; i < B; i++)
+//            {
+//                A = (A*A);
+//            return pr;
+//            }
+//        }
+//Console.WriteLine("Enter BASE A");
+//int A = int.Parse(Console.ReadLine()!);
+//Console.WriteLine("Enter EXPONENT B");
+//int B = int.Parse(Console.ReadLine()!);
+//int resalt = Exp(A);
+//Console.WriteLine(resalt);
+
+            Console.Write("Введите A: ");
+            int num = int.Parse(Console.ReadLine()!);
+            //int S = 1;
+            Console.Write("Введите кол-во повторов B: ");
+            int n = int.Parse(Console.ReadLine()!);
+            int num_n = 1;
+            for (int i = 0; i < n; i++)
+            {
+                num_n*= num;
+            }
+            Console.WriteLine(num_n);
