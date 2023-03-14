3eb45d85ed78845cb71001e9c3bb5214723569af
Alex <amkulikov@gmail.com>
2023-03-14 18:26:43 +0300

    5-Lesson

--- Lesson_5/5_5/Program.cs
+++ Lesson_5/5_5/Program.cs
@@ -5,42 +5,42 @@
 // [6 7 3 6] -> 36 21
 void Print(int[] arr) // берет массив и выводит его в строку с пробелами
 {
-    int size = arr.Length;
+    int size = arr.Length; // определяем размер массива, созданного в цикле 2
 
-    for (int i = 0; i < size; i++)
-        Console.Write($"{arr[i]} ");
-    Console.WriteLine();
+    for (int i = 0; i < size; i++) 
+        Console.Write($"{arr[i]} "); // чтение и вывод массива - интерполяция в строку
+    Console.WriteLine(); // пробел между массивами (если их несколько)
 }
 
-int[] MassNums(int size, int from, int to)
+int[] MassNums(int size, int from, int to) // формирует массив случайных чисел
 {
-    int[] arr = new int[size];
+    int[] arr = new int[size]; // указываем на формирование нового массива
 
     for (int i = 0; i < size; i++)
         arr[i] = new Random().Next(from, to);
     return arr;
 }
 
-int[] PairsNum(int[] arr)
+int[] PairsNum(int[] arr) // операции с массивом
 {
-    int size = arr.Length;
-    int flex_size = size / 2 + size % 2;
-    int[] new_arr = new int[flex_size];
+    int size = arr.Length; // вводим "старый" массив
+    int flex_size = size / 2 + size % 2; // определяем длину нового массивв
+    int[] new_arr = new int[flex_size]; // новый массив с "00" по всей длине - заготовка
 
-    for (int i = 0; i < size / 2; i++)
+    for (int i = 0; i < size / 2; i++) // цикл для массивов с четным кол.членов, для умножения крайних
         new_arr[i] = arr[i] * arr[size - i - 1];
     
-    if (size % 2 == 1)
+    if (size % 2 == 1) // условие записи среднего номера для нечетных массивов
         new_arr[flex_size - 1] = arr[size / 2];
     return new_arr;
 }
 
-int num = int.Parse(Console.ReadLine()!);
-int start = int.Parse(Console.ReadLine()!);
-int stop = int.Parse(Console.ReadLine()!);
+int num = int.Parse(Console.ReadLine()!);    // ниже главная связка
+int start = int.Parse(Console.ReadLine()!);  // все 3 строки - передают атрибуты в массив
+int stop = int.Parse(Console.ReadLine()!);   // MassNum, а он в цикле 2 определяет size from to
 
-int[] arr = MassNums(num, start, stop); // массив, сформированный в ?-м цикле, передан ?
-                                          // на хранение и печать (цикл 1)  
-int[] new_arr = PairsNum(arr);
+int[] arr = MassNums(num, start, stop); // массив, сформированный во 2-м цикле, передан 
+                                          // на хранение и дальнейшие действия (циклы 3, 1)  
+int[] new_arr = PairsNum(arr);// новый массив цикл 3, воспринимаем и передаем функции
 Print(arr);
 Print(new_arr);
---
+++ Lesson_5/5_8_HW3_var2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 3: Задайте массив вещественных чисел. Найдите
+// разницу между максимальным и минимальным элементов массива;
+// Решена с подсказки Алексея. В 4-м методе застрял - смешал правила для Void и возвращаемых функций.
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+int[] selectionSort(int[] arr2)
+{
+    for (int i = 0; i < arr2.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i+1; j < arr2.Length; j++)
+            {
+            if(arr2[j] < arr2[minPosition]) minPosition = j;
+            }
+            int temporary = arr2[i];
+            arr2[i] = arr2[minPosition];
+            arr2[minPosition] = temporary;
+    }
+    return arr2;
+}   
+int DifMaxMin(int[] arr)
+{
+    int size = arr.Length;
+    int result = arr[^1] - arr[0];
+    return result;
+}
+
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+int[] arr2 = selectionSort(mass);
+Print(arr2);
+int dif = DifMaxMin(arr2);
+Console.WriteLine(dif);
