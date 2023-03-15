91c4df364ddd141b650dfb12e8655d497c551bdd
Alex <amkulikov@gmail.com>
2023-03-15 08:58:38 +0300

    Lesson_6_seminar

---
+++ Lesson_6/6_2/Program.cs
@@ -0,0 +1,11 @@
+﻿string convert(int num_1)
+{
+    string rezult = "";
+    while (num_1 > 0)
+    {
+        rezult = num_1 % 2 + rezult;
+        num_1 = num_1 / 2;
+    }
+    return rezult;
+    }
+Console.WriteLine (convert(7));
