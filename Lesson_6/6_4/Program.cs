e7b39d24963c3d7fcc6656f32ed24a94ec6ae677
Alex <amkulikov@gmail.com>
2023-03-17 21:46:24 +0300

    Lesson 6 6_4 двумерный массив с определением размера и границ изменчивости с консоли но без переноса

--- Lesson_6/6_4/Program.cs
+++ Lesson_6/6_4/Program.cs
@@ -2,53 +2,58 @@
 // копию заданного массива с помощью поэлементного
 // копирования. Усложняем. Использовать по возможности - бинарный массив.
 // void Print(int[] arr)
-void PrintArray(int[,] matr)
+void PrintArray(int[,] matrix)
 {
-    for (int i = 0; i < matr.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < matr.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{matr[i, j]} ");
+            Console.Write($"{matrix[i, j]} ");
         }
     Console.WriteLine();
     }
 }
-void FillArray(int[,] matr)
+void FillArray(int[,] matrix)
 {
     int start = int.Parse(Console.ReadLine()!);
     int stop = int.Parse(Console.ReadLine()!);
-        for (int i = 0; i < matr.GetLength(0); i++)
+        for (int i = 0; i < matrix.GetLength(0); i++)
         {
-            for (int j = 0; j < matr.GetLength(1); j++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    matr[i,j] = new Random().Next(start,stop);
+                    matrix[i,j] = new Random().Next(start,stop);
                 }
         }
-        
+        Console.WriteLine(matrix);
 }
-void CopyMatrix(int[,] matr)
-{
-        //int[,] matr = new int[num1,num2];
-        for (int i = 0; i < matr.GetLength(0); i++)
-        {
-            for (int j = 0; j < matr.GetLength(1); j++)
-            {
-                matr[i,j] = matr2[i,j];
-                Console.Write(matr2[i,j] + " ");
-            }
-        Console.WriteLine();
-        }
-}
-int start = int.Parse(Console.ReadLine()!);
-int stop = int.Parse(Console.ReadLine()!);
+//void CopyMatrix(int[,] matrix2)
+//{
+//         Array matrix2 = 
+//         for (int i = 0; i < matrix.GetLength(0); i++)
+//         {
+//             for (int j = 0; j < matrix.GetLength(1); j++)
+//             {
+//                 matrix = new int[i,j];
+//                 Console.Write(matrix[i,j] + " ");
+//             }
+//         }
+//         Console.WriteLine();
+// }
+//int start = int.Parse(Console.ReadLine()!);
+//int stop = int.Parse(Console.ReadLine()!);
 int num1 = int.Parse(Console.ReadLine()!);
 int num2 = int.Parse(Console.ReadLine()!);
 int[,] matrix = new int[num1, num2];
-
+//int[,] matrix2 = new int[num1, num2];
 //int[,] matr = new int[i,j];
 PrintArray(matrix);
 FillArray(matrix);
 Console.WriteLine();
 PrintArray(matrix);
-CopyMatrix(matrix);
+//CopyMatrix(matrix2);
 Console.WriteLine();
+// Ниасилил..Код в 2 этапа делает матрицу n/m и заполняет ее случайными числами. 
+// В 1 этап, чтобы сразу ввести количество строк и столбцов, и границы случайных чисел,
+// не смог, но хоть работает. А переписать поэлементным копированием через цикл 
+// в новый массив - не смог... Не могу определить массив -строка 3. И объединением 
+// двух void-ов с циклами в один тоже не получается.
