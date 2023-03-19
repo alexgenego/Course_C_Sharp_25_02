c353c14072df33fe7c335691300962382e306627
Alex <amkulikov@gmail.com>
2023-03-19 09:35:28 +0300

    I_don_t_know_what

--- Lesson_4/4_4_HW/Program.cs
+++ Lesson_4/4_4_HW/Program.cs
@@ -1,18 +1,18 @@
 ﻿// Задача 1: Напишите цикл, который принимает на вход два числа 
 // (A и B) и возводит число A в натуральную степень B.
 
-//Console.WriteLine("Enter BASE A");      //перенесены снизу.
-//int A = int.Parse(Console.ReadLine()!); //перенесены снизу.
-//Console.WriteLine("Enter EXPONENT B");  //перенесены снизу.
-//int B = int.Parse(Console.ReadLine()!); //перенесены снизу.
-//int Exp = 1;
-//for (int i = 0; i < B; i++)
+// Console.WriteLine("Enter BASE A");      //перенесены снизу.
+// int A = int.Parse(Console.ReadLine()!); //перенесены снизу.
+// Console.WriteLine("Enter EXPONENT B");  //перенесены снизу.
+// int B = int.Parse(Console.ReadLine()!); //перенесены снизу.
+// int Exp = 1;
+// for (int i = 0; i < B; i++)
 //        {
 //            Exp *= A;
 //        }
 //        return Exp; - обрывает на первом шаге.
 
-//Console.WriteLine(Exp);
+// Console.WriteLine(Exp);
 
     Console.Write("Введите A: ");
     int num = int.Parse(Console.ReadLine()!);
