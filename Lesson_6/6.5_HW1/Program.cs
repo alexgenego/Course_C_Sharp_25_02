f178b33c572ed2d9915f3deb27762c32d25bfd20
Alex <amkulikov@gmail.com>
2023-03-19 09:49:08 +0300

    6_5_HW1

--- Lesson_6/6.5_HW1/Program.cs
+++ Lesson_6/6.5_HW1/Program.cs
@@ -1,24 +1,31 @@
 ﻿// Задача 1: Пользователь вводит с клавиатуры M чисел.
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-string a;
-int[] b = new int[100];
-int i=0;
- 
+
+int[] FillMass()
+{
+    string a;
+    int[] b = new int[100];
+    int i = 0;
+
     while ((a = Console.ReadLine()!) != "end") //Получаем строку и проверяем ее содержание "end".
         b[i++] = Convert.ToInt32(a); //в строке нет "конца" - значит конвертим и пишем в массив
-    for (int k = 0; k < i; k++) //вывод заполненных ячеек массива
-    Console.Write($"{b[k]} ");
-    
+    // for (int k = 0; k < i; k++) //вывод заполненных ячеек массива
+    //     Console.Write($"{b[k]} ");
+    return b;
+}
+
 int PosMassiv(int[] b)
 {
-        //int[] b = b[i];
-        // for (int i = 0 ; i < b.Length ; i++)
-        //     if (b[i] > 0);
-        //     i++;
-        //     return i;
-        return b.Length; 
-                  
+    int count = 0;
+    for (int i = 0; i < b.Length; i++)
+        if (b[i] > 0)
+            count++;
+
+    return count;
 }
-Console.WriteLine(b.Length);             
-Console.ReadKey(true); //ключ ввода с клавиатуры
+
+
+int[] new_mass = FillMass();
+int result = PosMassiv(new_mass);
+Console.WriteLine(result);
