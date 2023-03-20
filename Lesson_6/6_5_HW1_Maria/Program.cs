338ecd7bdcd6b65399e9eaec82a0d5ebd6bf6cb2
Alex <amkulikov@gmail.com>
2023-03-20 12:18:15 +0300

    Lesson6 работа над оибками

---
+++ Lesson_6/6_5_HW1_Maria/Program.cs
@@ -0,0 +1,45 @@
+﻿// Ввод с клавиатуры (словно) чисел и число положительных из них.
+
+int Prompt (string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.Total32(value);
+    return result;
+}
+int[] InputArray(int lenght)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"Введите {i + 1}-й элемент");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for ( int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[1]}");
+    }
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int lenght = Prompt("Введите количество элементов >");
+int[] array;
+array = InputArray(lenght);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше 0 - {CountPositiveNumbers(array)}");
