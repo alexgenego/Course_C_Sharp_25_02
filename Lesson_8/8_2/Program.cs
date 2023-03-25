dd77e79359f416fd259697de1c4c4c7e615db2ed
Alex <amkulikov@gmail.com>
2023-03-25 08:49:52 +0300

    Lesson_8_HW+start

--- Lesson_8/8_2/Program.cs
+++ Lesson_8/8_2/Program.cs
@@ -2,56 +2,111 @@
 // Частотный словарь содержит информаци о том, сколько раз встречается элемент входных данных.
 // Значения элементов массива 0...9.
 
+// void Print(int[,] arr)
+// {
+//   int row = arr.GetLength(0);
+//   int column = arr.GetLength(1);
+
+//   for (int i = 0; i < row; i++)
+//   {
+//     for (int j = 0; j < column; j++)
+//       Console.Write($" {arr[i, j]} ");
+//     Console.WriteLine();
+//   }
+//   Console.WriteLine();
+// }
+
+// int[,] MassNums(int row, int column, int from, int to)
+// {
+//   int[,] arr = new int[row, column];
+
+//   for (int i = 0; i < row; i++)
+//     for (int j = 0; j < column; j++)
+//       arr[i, j] = new Random().Next(from, to);
+
+//   return arr;
+// }
+
+// int[] FrequencyDictionary(int[,] arr)
+// {
+//   int[] arr2 = new int[10];
+//   foreach (int a in arr) arr2[a]++;
+//   return arr2;
+// }
+
+// void Print2(int[] arr)
+// {
+//   for (int i = 0; i < arr.Length; i++)
+//     Console.WriteLine($" {i} - {arr[i]} ");
+//   Console.WriteLine();
+// }
+
+
+// Console.WriteLine("Введите количество строк массива");
+// int num_row = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите количество колонок массива");
+// int num_column = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Задайте минимальное число в массиве");
+// int start = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Задайте максмальное число в массиве");
+// int stop = int.Parse(Console.ReadLine()!);
+
+
+// int[,] mass = MassNums(num_row, num_column, start, stop);
+// Print(mass);
+// Print2(FrequencyDictionary(mass));
+// PrintMass(new_mass);
+
 void Print(int[,] arr)
 {
-  int row = arr.GetLength(0);
-  int column = arr.GetLength(1);
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
 
-  for (int i = 0; i < row; i++)
-  {
-    for (int j = 0; j < column; j++)
-      Console.Write($" {arr[i, j]} ");
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],3} ");
+        Console.WriteLine();
+    }
     Console.WriteLine();
-  }
-  Console.WriteLine();
 }
 
 int[,] MassNums(int row, int column, int from, int to)
 {
-  int[,] arr = new int[row, column];
+    int[,] arr = new int[row, column];
 
-  for (int i = 0; i < row; i++)
-    for (int j = 0; j < column; j++)
-      arr[i, j] = new Random().Next(from, to);
-
-  return arr;
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
 }
 
-int[] FrequencyDictionary(int[,] arr)
+int[] FrequencyDict(int[,] arr, int max_num)
 {
-  int[] arr2 = new int[10];
-  foreach (int a in arr) arr2[a]++;
-  return arr2;
+    int[] freq = new int[max_num];
+
+    foreach (int item in arr) freq[item] += 1;
+    return freq;
 }
 
-void Print2(int[] arr)
+void PrintMass(int[] arr)
 {
-  for (int i = 0; i < arr.Length; i++)
-    Console.WriteLine($" {i} - {arr[i]} ");
-  Console.WriteLine();
+    for (int i = 0; i < arr.Length; i++)
+        Console.WriteLine($"{i} meets: {arr[i]}");
+    Console.WriteLine();
 }
 
 
-Console.WriteLine("Введите количество строк массива");
-int num_row = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите количество колонок массива");
-int num_column = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Задайте минимальное число в массиве");
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
 int start = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Задайте максмальное число в массиве");
 int stop = int.Parse(Console.ReadLine()!);
+int[,] mass = MassNums(row_num, column_num, start, stop);
 
-
-int[,] mass = MassNums(num_row, num_column, start, stop);
 Print(mass);
-Print2(FrequencyDictionary(mass));
+
+int[] new_mass = FrequencyDict(mass, stop);
+PrintMass(new_mass);
