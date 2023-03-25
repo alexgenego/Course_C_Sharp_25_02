28392db8e3b8b162d312d10b24a01b938ad560a9
Alex <amkulikov@gmail.com>
2023-03-25 15:41:59 +0300

    Lesson_9_seminar

---
+++ Lesson_9/9_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// 3. Напишите программу, которая будет принимать на вход число
+//    и возвращать сумму его цифр. Использовать рекурсию.
+
+
+// int ValuesSet(int N)
+// {
+//     if (N == 0) return 0;
+//     return ValuesSet(N / 10) + N % 10;
+// }
+
+// int num = int.Parse(Console.ReadLine()!);
+// ValuesSet(num1, num2);
+// Console.WriteLine(ValuesSet(num));
+
+
+
+int ValueSet(int N)
+{
+    if (N == 0) return 0;
+    return ValueSet(N / 10) + N % 10;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine(ValueSet(num));
