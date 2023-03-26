825588bb5776d59a46445789284a1bbb652dd94f
Alex <amkulikov@gmail.com>
2023-03-26 10:01:07 +0300

    L_9

--- Lesson_9/9_4/Program.cs
+++ Lesson_9/9_4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные
+// натуральные числа в промежутке от M до N с помощью рекурсии.
+
+void ValuesSet(int M, int N)
+{
+    if (N < M) return;
+    ValuesSet(M , N- 1);
+    Console.Write($"{N} ");
+}
+
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+ValuesSet(num1, num2);
